aa7401793e43b630f36feb59559dc433e6eecfa1
Sergey Dvoretskiy <umbrella18@mail.ru>
2023-07-05 09:44:52 +0300

    добавил памятку 1

--- homework/hw.py
+++ homework/hw.py
@@ -1,14 +1,17 @@
 #  Варианты реализации циклов
 
-my_list = []
-n = int(input('Что-то вводим: '))
-for _ in range(n):
-    my_list.append(int(input('еще что-то вводим')))
+# my_list = []
+# n = int(input('Что-то вводим: '))
+# for _ in range(n):
+#     my_list.append(int(input('еще что-то вводим')))
 
 
-#  То же самое одной строкой
+# #  То же самое одной строкой
 
-n = int(input('что-то вводим: '))
-string = input('что-то вводим еще: ')
+# n = int(input('что-то вводим: '))
+# string = input('что-то вводим еще: ')
+# ist = [int(i) for i in string.split()] # split разобьем и составит список из чисел
 
-ist = [int(i) for i in string.split()]
+
+ist = [i for i in range(1, 6) if i % 2 == 0]
+print(ist)
