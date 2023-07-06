bbf4275a76ef72425203137543f5bc95391f14ac
Sergey Dvoretskiy <umbrella18@mail.ru>
2023-07-06 10:17:08 +0300

    сделал ДЗ 4 семинара

--- lesson/less.py
+++ lesson/less.py
@@ -8,16 +8,26 @@
 # A = 3; B = 5 -> 243 (3⁵)
 # A = 2; B = 3 -> 8
 
-def func(a, b):
-    a1 = a
-    for _ in range(b-1):
-        a = a * a1
-    return a
+# def fact(a, b):
+#     a1 = a
+#     for _ in range(b-1):
+#         a = a * a1
+#     return a
+
+
+
+
+def fact(a, b):
+    if b == 0:
+        return 1
+    else:
+        return a * fact(a, b - 1)
 
 a = int(input('Введите число: '))
 b = int(input('В какую степень возводим?: '))
 
-print(func(a, b))
+print(fact(a, b))
+
 
 
 
@@ -30,3 +40,15 @@ print(func(a, b))
 # 2 2
 # 4
 
+
+def recursive(a, b,):
+    if b == 0:
+        return a
+    else:
+        return recursive(a + 1, b - 1)
+    
+
+a = int(input('Введите 1 число: '))
+b = int(input('Введите 2 число: '))
+
+print(recursive(a, b))
