7c1e896cede5e836148969e69615e223160f05f4
Sergey Dvoretskiy <umbrella18@mail.ru>
2023-07-06 09:16:42 +0300

    сделали с преподом задания 4 семинара

--- lesson/less.py
+++ lesson/less.py
@@ -37,6 +37,26 @@
 # print(fib(7))
 
 
+#  Варианты реализации циклов
+
+# my_list = []
+# n = int(input('Что-то вводим: '))
+# for _ in range(n):
+#     my_list.append(int(input('еще что-то вводим')))
+
+
+# #  То же самое одной строкой
+
+# n = int(input('что-то вводим: '))
+# string = input('что-то вводим еще: ')
+# ist = [int(i) for i in string.split()] # split разобьем и составит список из чисел
+
+
+# ist = [i for i in range(1, 6) if i % 2 == 0]
+# print(ist)
+
+
+
 #  ЗАДАЧИ
 
 
@@ -48,21 +68,43 @@
 # Output: 21
 # Задание необходимо решать через рекурсию
 
+# def fib(n):
+#     if n < 2:
+#         return n
+#     else:
+#         return fib(n - 1) + fib(n - 2)
 
+# a = [0,1]
 
-
-
-
-
+# for i in range(50):
+#     print(i, fib(i))
+# fib(5)
  
+# a = [0, 1]
+# for i in range(50):
+#     a.append(a[-1] + a[-2])
+#     print(i, a[-1])
+
+
+
+
+
 # Задача №33. Решение в группах
 # Хакер Василий получил доступ к классному журналу и хочет заменить все свои минимальные оценки на максимальные. 
 # Напишите программу, которая заменяет оценки Василия, но наоборот: все максимальные – на минимальные.
 # Input: 5 -> 1 3 3 3 4
 # Output: 1 3 3 3 1
 
-
-
+# list = [1, 3, 3, 3, 4, 4]
+# min_lst = min(list)
+# max_lst = max(list)
+# new_lst = []
+# for i in list:
+#     if i == max_lst:
+#         new_lst.append(min_lst)
+#     else:
+#         new_lst.append(i)
+# print(new_lst)
 
 
 
@@ -74,8 +116,15 @@
 # Output: yes
 
 
-
-
+def simple_number(num):
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    else:
+        return True
+    
+a = int(input('Введите число: '))
+print(simple_number(a))
 
 
 
